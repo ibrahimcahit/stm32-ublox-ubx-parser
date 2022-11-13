bef56d65e3091e829d4bed5370fda604a36e25a4
Ibrahim Ozdemir <53236678+ibrahimcahit@users.noreply.github.com>
2022-11-13 04:46:32 +0300

    Add files via upload

---
+++ Core Library/main.c
@@ -0,0 +1,260 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <GNSS.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+GNSS_StateHandle GNSS_Handle;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int _write(int file, char *ptr, int len)
+{
+	HAL_UART_Transmit(&hlpuart1, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+	return len;
+}
+int count, countMax;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_LPUART1_UART_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  	// Start to configure GNSS module. Set baudrate to 115200, disable NMEA, set rate to 5 Hz, etc.
+	GNSS_Init(&GNSS_Handle, &huart1);
+
+	// Because we changed GNSS baud rate to 115200, we need to change also UART1 baud rate and init it again.
+	HAL_UART_Abort_IT(&huart1);
+	HAL_UART_DeInit(&huart1);
+	huart1.Init.BaudRate = 115200;
+	if (HAL_UART_Init(&huart1) != HAL_OK) {
+	    Error_Handler();
+	}
+	HAL_Delay(1000);
+
+	// Timer values for GNSS requester and Hz counter
+	uint32_t Timer = HAL_GetTick();
+	uint32_t TimeHz = HAL_GetTick();
+	int printFlag = 0;
+
+	// Set GNSS mode to desired aplication case
+	GNSS_SetMode(&GNSS_Handle, Stationary);
+
+	HAL_Delay(250);
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	// Request GNSS data whenever timer hits 100 ms.
+	// As HAL_GetTick returns SysTime as milliseconds, 100 ms means we are getting
+	// 		GNSS values at 5 Hz rate
+	if ((HAL_GetTick() - Timer) > 200) {
+
+		// Request "Navigation Position Velocity Time Solution"
+		// Refeer to "32.17.17 UBX-NAV-PVT (0x01 0x07)" at M8N Interface manual
+		GNSS_GetPVTData(&GNSS_Handle);
+		GNSS_ParseBuffer(&GNSS_Handle);
+
+		Timer = HAL_GetTick();
+
+		printFlag = 1;
+		count ++;
+	}
+
+	// As printing to external com port is time consuming, we are doing this task
+	//		outside our 5 Hz time-dependent loop
+	if (printFlag == 1){
+		printf("Day: %d-%d-%d \r\n", GNSS_Handle.day, GNSS_Handle.month,GNSS_Handle.year);
+		printf("Time: %d:%d:%d \r\n", GNSS_Handle.hour, GNSS_Handle.min,GNSS_Handle.sec);
+		printf("Status of fix: %d \r\n", GNSS_Handle.fixType);
+		printf("Number of satellites used: %d \r\n", GNSS_Handle.numSV);
+		printf("Number of Sat.: %d \r\n", GNSS_Handle.satCount);
+		printf("Latitude: %f \r\n", GNSS_Handle.fLat);
+		printf("Longitude: %f \r\n",(float) GNSS_Handle.lon / 10000000.0);
+		printf("Height above ellipsoid (meters): %f \r\n", (float) (GNSS_Handle.height) / 1000.0);
+		printf("Height above mean sea level (meters): %f \r\n", (float) (GNSS_Handle.hMSL) / 1000.0);
+		printf("Ground Speed (2-D): %ld \r\n", GNSS_Handle.gSpeed);
+		printf("***************************************************\r\n");
+
+		printFlag = 0;
+	}
+
+	if ((HAL_GetTick() - TimeHz) > 1000) {
+		printf("Hz rate is: %d, time is: %lu \r\n", count, TimeHz);
+		printf("***************************************************\r\n");
+		countMax = count;
+		count = 0;
+		TimeHz = HAL_GetTick();
+	}
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV4;
+  RCC_OscInitStruct.PLL.PLLN = 85;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
